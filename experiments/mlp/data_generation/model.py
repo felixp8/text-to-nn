24b4b8b5452e51577e9c5d9859d76b90611ea1ed
Felix Pei <64850082+felixp8@users.noreply.github.com>
2023-06-26 18:54:53 -0400

    add dropout to MLP

--- experiments/mlp/data_generation/model.py
+++ experiments/mlp/data_generation/model.py
@@ -4,18 +4,28 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 class MLP(nn.Module):
-    def __init__(self, input_dim: int, hidden_dims: list, output_dim: int, activation: str = "relu", bias=True):
+    def __init__(
+        self, 
+        input_dim: int, 
+        hidden_dims: list, 
+        output_dim: int, 
+        activation: str = "relu", 
+        bias: bool = True,
+        dropout: float = 0.0,
+    ):
         super().__init__()
         dims = [input_dim] + hidden_dims + [output_dim]
 
-        if activation == "relu":
+        if activation.lower() == "relu":
             activation = nn.ReLU
-        elif activation == "sigmoid":
+        elif activation.lower() == "sigmoid":
             activation = nn.Sigmoid
-        elif activation == "tanh":
+        elif activation.lower() == "tanh":
             activation = nn.Tanh
-        elif activation == "gelu":
+        elif activation.lower() == "gelu":
             activation = nn.GELU
+        elif activation.lower() == "silu":
+            activation == nn.SiLU
         else:
             raise ValueError()
 
@@ -23,6 +33,7 @@ class MLP(nn.Module):
         for i in range(len(dims) - 2):
             layerlist.append(nn.Linear(dims[i], dims[i+1], bias=bias))
             layerlist.append(activation())
+            layerlist.append(nn.Dropout(dropout))
         layerlist.append(nn.Linear(dims[-2], dims[-1], bias=True))
 
         self.layers = nn.Sequential(*layerlist)
